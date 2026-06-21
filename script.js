9627e757933c1a52d80950b5e2d97873b1344b8a
gugu-codes <ipshu.upreti@gmail.com>
2026-06-21 21:13:04 +0545

    Completed adding additional info of remaining trails

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-
 const trailInfo = {
+    Sundarijal: {
+        elevation: "1350 m",
+        season: "All year",
+        highlight: "Gateway to Shivapuri National Park"
+    },
+
+    Mulkharka: {
+        elevation: "1800 m",
+        season: "Spring, Autumn",
+        highlight: "Traditional Tamang village"
+    },
+
+    Manichud: {
+        elevation: "2400 m",
+        season: "Spring, Autumn",
+        highlight: "Forest trails and hilltop views"
+    },
+
     Chisapani: {
         elevation: "2165 m",
         season: "Spring, Autumn",
@@ -12,13 +29,48 @@ const trailInfo = {
         highlight: "Himalayan panorama"
     },
 
+    Dhulikhel: {
+        elevation: "1550 m",
+        season: "All year",
+        highlight: "Scenic mountain viewpoints"
+    },
+
+    "Dhap Dam": {
+        elevation: "1900 m",
+        season: "Autumn, Winter",
+        highlight: "Peaceful reservoir and birdwatching"
+    },
+
+    "Shivapuri Peak": {
+        elevation: "2732 m",
+        season: "Spring, Autumn",
+        highlight: "Highest peak around Kathmandu Valley"
+    },
+
+    "Nagi Gumba": {
+        elevation: "2330 m",
+        season: "All year",
+        highlight: "Buddhist monastery and forest views"
+    },
+
+    Jamacho: {
+        elevation: "2128 m",
+        season: "Spring, Autumn",
+        highlight: "Tower viewpoint inside Nagarjun Forest"
+    },
+
+    Champadevi: {
+        elevation: "2285 m",
+        season: "Spring, Autumn",
+        highlight: "Popular ridge hike with valley views"
+    },
+
     Phulchoki: {
         elevation: "2782 m",
         season: "Spring",
-        highlight: "Rhododendron forests"
+        highlight: "Rhododendron forests and birdwatching"
     }
 };
-
 const locations = {
     Sundarijal: [27.7810, 85.4210],
     Mulkharka: [27.8000, 85.4100],
